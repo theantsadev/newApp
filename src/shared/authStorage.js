6103f5c0460f1165f64971e610deb5c70b327c11
MichaelETU003327 <mikermb18@gmail.com>
2026-05-14 08:25:00 +0300

    Update authStorage.js

--- src/shared/authStorage.js
+++ src/shared/authStorage.js
@@ -5,11 +5,11 @@ const STORAGE_KEY = "prestashop_api_key";
 export const getStoredApiKey = () => localStorage.getItem(STORAGE_KEY) || "";
 
 export const setStoredApiKey = (apiKey) => {
-  localStorage.setItem(STORAGE_KEY, apiKey);
+    localStorage.setItem(STORAGE_KEY, apiKey);
 };
 
 export const clearStoredApiKey = () => {
-  localStorage.removeItem(STORAGE_KEY);
+    localStorage.removeItem(STORAGE_KEY);
 };
 
 export const verifyApiKey = (apiKey) => apiKey === PRESTASHOP_API_KEY;
